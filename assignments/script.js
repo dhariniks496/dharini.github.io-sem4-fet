b2e105e5d998a88d670551838b958b917ecb4492
Dharini KS <dhariniks311@gmail.com>
2026-04-25 12:40:35 +0530

    Update changeDetails function for validation

--- assignments/script.js
+++ assignments/script.js
@@ -2,13 +2,13 @@ function changeDetails() {
     let newName = prompt("Enter your new name:");
     let newDetail = prompt("Enter your new details:");
 
-    if (newName) {
-        document.getElementById("name").innerText = newName;
+    if (!newName || !newDetail) {
+        alert("Details not updated! PLEASE ENTER!");
+        return;
     }
 
-    if (newDetail) {
-        document.getElementById("details").innerText = newDetail;
-    }
+    document.getElementById("name").innerText = newName;
+    document.getElementById("details").innerText = newDetail;
 
     alert("Details updated!");
 }
