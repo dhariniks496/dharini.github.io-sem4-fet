cebcaf3d96048dc042e233ade67901e91ac47540
Dharini KS <dhariniks311@gmail.com>
2026-04-25 12:58:25 +0530

    Refactor changeDetails function for validation

--- assignments/script.js
+++ assignments/script.js
@@ -2,7 +2,18 @@ function changeDetails() {
     let newName = prompt("Enter your new name:");
     let newDetail = prompt("Enter your new details:");
 
-    if (!newName || !newDetail) {
+   
+    if (!newName) {
+        alert("Name not updated! PLEASE ENTER!");
+        return;
+    }
+
+    if (newName !== newName.toUpperCase()) {
+        alert("Please enter the name in CAPS!");
+        return;
+    }
+
+    if (!newDetail) {
         alert("Details not updated! PLEASE ENTER!");
         return;
     }
